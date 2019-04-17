4b861f6351dcacfc40e14179942b0733bec6d96e
Dipanjan Bhowmik <onlydevelop@gmail.com>
2019-04-17 16:36:17 +0530

    Separated web/console path

--- server.js
+++ server.js
@@ -14,8 +14,29 @@ app.get('/', function (req, res) {
   res.send('Hello world\n');
 });
 
-app.get('/user/:id', function (req, res) {
-  res.send('Hello ' + req.params.id + ' from ' + hostname);
+app.get('/console/:user', function (req, res) {
+  var date = new Date();
+  var response = `Hello ${req.params.user} from ${hostname} at ${date}\n
+`;
+  res.send(response);
+});
+
+app.get('/web/:user', function (req, res) {
+  var date = new Date();
+  var bgcolor = "#FFFFFF"
+
+  if (/[a-f][0-9]+/.test(hostname)) {
+    bgcolor = `#${hostname.slice(0, 6)}`;
+  }
+  var response = `<html>
+  <body bgcolor="${bgcolor}">
+    <h2>Hello ${req.params.user}!</h2>
+    <h3>I am running on: ${hostname}</h3>
+    <h3>Current time: ${date} </h3>
+  </body>
+</html>
+`;
+  res.send(response);
 });
 
 app.listen(PORT);
